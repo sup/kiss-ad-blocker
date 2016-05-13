8c888e20f89a8194ac413137d7b1793760c11dae
Charles Lai <cjl223@cornell.edu>
2016-05-13 11:17:50 -0400

    remove sidebar and banner ads as well

--- kiss-ad-blocker/src/inject/inject.js
+++ kiss-ad-blocker/src/inject/inject.js
@@ -1,25 +1,45 @@
 chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "interactive") {
-		clearInterval(readyStateCheckInterval);
+  var readyStateCheckInterval = setInterval(function() {
+    // Wait for interactive for best UX
+    if (document.readyState === "interactive") {
+      clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// Click on hide button to hide "hidable" ads
-		setTimeout(hideSimple, 2000);
+      // Always hide banner and sidebar ads
+      hideBanner();
+      hideSidebar();
 
-		// TODO: Block Banner Ads
-
-		// ----------------------------------------------------------
-
-	}
-	}, 10);
+      // Hide hideabe ads with a small offset to avoid Kiss* ad block detection
+      setTimeout(hideSimple, 1000);
+    }
+  }, 10);
 });
 
+// Hides simple ads
 function hideSimple() {
-	var hideButtons = document.getElementsByClassName("divCloseBut");
-	for (var key in hideButtons) {
-		while (hideButtons.hasOwnProperty(key)) {
-			hideButtons[key].firstChild.click();
-		}
-	}
+  var hideButtons = document.getElementsByClassName("divCloseBut");
+  for (var key in hideButtons) {
+    while (hideButtons.hasOwnProperty(key)) {
+      hideButtons[key].firstChild.click();
+    }
+  }
+}
+
+// Hides banner ads
+function hideBanner() {
+  var banners = document.querySelectorAll('[id^="divAds"]');
+  for (var key in banners) {
+    if (banners.hasOwnProperty(key)) {
+      banners[key].remove();
+    }
+  }
+}
+
+// Hides sidebar ads
+function hideSidebar() {
+  var sidebars = document.querySelectorAll('[id^="BB_"]');
+  for (var key in sidebars) {
+    if (sidebars.hasOwnProperty(key)) {
+      sidebars[key].remove();
+    }
+  }
 }
